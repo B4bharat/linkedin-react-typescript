835635692ab6e5c9a4fb7412e06082afdcf31e9c
Bharat Poptwani <b4bharat.available@gmail.com>
2020-10-26 19:19:58 +0530

    interface introduction

--- src/index.tsx
+++ src/index.tsx
@@ -7,13 +7,22 @@ import Message from "./Message";
 
 import "./styles.css";
 
-class App extends React.Component<any> {
+const initialState = {
+  name: "Manny",
+  message: "Typescript is cool!!"
+};
+
+type State = Readonly<typeof initialState>;
+
+class App extends React.Component<any, State> {
+  readonly state: State = initialState;
+
   render() {
     return (
       <div className="App">
         <h1>Hello React TSX</h1>
         <h2>TypeScript now works in CRA!</h2>
-        <Message name="Bharat" message="this is a simple message" />
+        <Message name={this.state.name} message={this.state.message} />
 
         <Hello type="TypeScript" />
         <BarChart />
