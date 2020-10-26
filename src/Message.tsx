23804a9281f6e139552f6b9f4844f61ce3532441
Bharat Poptwani <b4bharat.available@gmail.com>
2020-10-26 13:05:09 +0530

    created a Message Component

---
+++ src/Message.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Message = () => {
+  return <div>This is a Message</div>;
+};
+
+export default Message;
--- src/index.tsx
+++ src/index.tsx
@@ -3,30 +3,16 @@ import * as ReactDOM from "react-dom";
 
 import Hello from "./Hello";
 import BarChart from "./BarChart";
+import Message from "./Message";
 
 import "./styles.css";
 
 function App() {
-  // Tuple
-  let aTuple: [string, number] = ["Manny", 34];
-  // enum
-  enum Codes {
-    first = 1,
-    second = 2
-  }
-  // void
-  const warning = (): void => {
-    console.log("Warning");
-  };
-
   return (
     <div className="App">
       <h1>Hello React TSX</h1>
       <h2>TypeScript now works in CRA!</h2>
-      <p>
-        {" "}
-        The value {aTuple[0]} is of {typeof aTuple[0]} type!
-      </p>
+      <Message />
 
       <Hello type="TypeScript" />
       <BarChart />
